ae67bc702b7e71f0db9c275a541913a21c8ba8cc
themirrazz <89490659+themirrazz@users.noreply.github.com>
2025-02-24 08:49:35 -0800

    Update fandom-script.js

--- fandom-script.js
+++ fandom-script.js
@@ -1,10 +1,13 @@
 // LICENSE https://github.com/kyosukeweb/honorifics.js/blob/main/LICENSE.md
 // FandomScript
-// Easily update any text
-setInterval(function () {
+
+// Fandom takes a while to load Common.js or Personal JS
+// We don't need setInterval here
+(function () {
     var query = window.__HonorificsElementContainers__ || '[data-honorific]';
     var all = Array.from(document.querySelectorAll(query));
     for(var i = 0; i < all.length; i++) {
+        // Fandom doesn't support <element hidden />
         all[i].style.display = navigator.honorifics ? "" : "none";
     }
-}, window.__HonorificsRefreshTime__ || 100);
+})();
